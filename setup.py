ba2f00c087b6ee040d3c77bcd992cce32e2e3082
Raphael Schubert <rfswdp@gmail.com>
2019-01-22 04:21:08 -0200

    updated package url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='schubert_serpro',
     version='0.0.1',
     description='Consulta rapida de CPF, CNPJ nas bases federais da SERPRO',
-    url='git@github.com:rfschubert/ptolemaios-sdk-package.git',
+    url='https://schubert.gitbook.io/serpro/',
     author='Raphael Schubert',
     author_email='rfswdp@gmail.com',
     license='MIT',
