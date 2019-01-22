2d457a1a8f9167a61e07a6d72d7c9a1867940dfb
Raphael Schubert <rfswdp@gmail.com>
2019-01-22 03:22:51 -0200

    added .gitignore, basic config to deploy pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='schubert_serpro',
+    version='0.0.1',
+    description='Consulta rapida de CPF, CNPJ nas bases federais da SERPRO',
+    url='git@github.com:rfschubert/ptolemaios-sdk-package.git',
+    author='Raphael Schubert',
+    author_email='rfswdp@gmail.com',
+    license='MIT',
+    packages=['schubert_serpro'],
+    zip_safe=False
+)
